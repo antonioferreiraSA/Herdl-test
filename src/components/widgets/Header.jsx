92a318ca87d76aeaa613d7372f2720e7fc349aaa
Antonio Ferreira <antonio@makebetter.co.za>
2023-02-19 22:53:43 +0200

    Datocms final touches

---
+++ src/components/widgets/Header.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default async function Header() {
+  // useEffect(() => {
+  //   const data = getHeading();
+  //   console.log('eseEffect data:', data);
+  //   setHeading(data);
+  // }, []);
+
+  return (
+    <>
+      <div>Header is here </div>
+    </>
+  );
+}
